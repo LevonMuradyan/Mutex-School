8dd224fa10e1bfc31c9259c0ab82bd38aa7adc91
LevonMuradyan <levon.muradyan9559@gmaial.com>
2022-06-06 02:02:56 +0400

    VM 0.1

--- Virtual-Machine/src/cpu.cpp
+++ Virtual-Machine/src/cpu.cpp
@@ -1,27 +1,65 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <bitset>
 
 #include "cpu.h"
 
-CPU::CPU() : m_inReg(0), m_outReg(0), m_reg0(0), m_reg1(0),
-    m_reg2(0), m_reg3(0), m_reg4(0), m_reg5(0), isCondition(false),
-    m_counter(0)
+CPU::CPU() : m_counter(0), m_input(0), m_output(0)
 {
-
+	m_regs = std::vector<int32_t>(6);
 }
 
 void CPU::read(char *fileName)
 {
     std::ifstream input(fileName, std::ios::binary);
-    m_ram = std::vector<int8_t>(std::istreambuf_iterator<char>(input), {});
+	m_ram = std::vector<int8_t>(std::istreambuf_iterator<char>(input), {});
 
-    for (auto& e: m_ram) {
-        std::cout << +e << std::endl;
-    }
+	for (size_t i = 0; i < m_ram.size(); ++i) {
+		std::bitset<8> b1(m_ram[i]);
+		std::cout << b1 << std::endl;
+	}
 }
 
 void CPU::process()
 {
+	while (m_regs[COUNTER] + 3 < (int8_t)m_ram.size()) {
+		int8_t opcode = +m_ram[m_regs[COUNTER]];
+		int8_t arg1 = +m_ram[m_regs[COUNTER] + 1];
+		int8_t arg2 = +m_ram[m_regs[COUNTER] + 2];
+		int8_t dest = +m_ram[m_regs[COUNTER] + 3];
+
+		int8_t value1 = opcode != (opcode & (~(1 << 7))) ? arg1 : m_regs[arg1];
+
+		bool cond = false;
+
+		switch (opcode) {
+		case EQ:
+			cond = m_regs[arg1] == m_regs[arg2];
+			break;
+		case NEQ:
+			cond = m_regs[arg1] != m_regs[arg2];
+			break;
+		case L:
+			cond = m_regs[arg1] < m_regs[arg2];
+			break;
+		case LE:
+			cond = m_regs[arg1] <= m_regs[arg2];
+			break;
+		case G:
+			cond = m_regs[arg1] > m_regs[arg2];
+			break;
+		case GE:
+			cond = m_regs[arg1] >= m_regs[arg2];
+			break;
+		}
+
+		if (opcode != (opcode & (~(1 << 5))) && cond) {
+			m_regs[COUNTER] = (uint8_t)dest;
+		} else {
+			m_regs[COUNTER] += 4;
+		}
+
+	}
 
 }
--- Virtual-Machine/src/cpu.h
+++ Virtual-Machine/src/cpu.h
@@ -4,6 +4,16 @@
 #include <stdint.h>
 #include <vector>
 
+#define REG0 0
+#define REG1 1
+#define REG2 2
+#define REG3 3
+#define REG4 4
+#define REG5 5
+#define COUNTER 6
+#define INPUT 7
+#define OUTPUT 8
+
 #define ADD 0
 #define SUB 1
 #define AND 2
@@ -27,20 +37,13 @@ public:
 
 private:
     // registers
-    int32_t m_inReg;
-    int32_t m_outReg;
-    int32_t m_reg0;
-    int32_t m_reg1;
-    int32_t m_reg2;
-    int32_t m_reg3;
-    int32_t m_reg4;
-    int32_t m_reg5;
-
-    bool isCondition;
-    uint32_t m_counter;
+	std::vector<int32_t> m_regs;
+	uint32_t m_counter;
+	int32_t m_input;
+	int32_t m_output;
 
     // ram buffer
-    std::vector<int8_t> m_ram;
+	std::vector<int8_t> m_ram;
 };
 
 #endif // CPU_H
